b05fd4edb330dcc6e5bef1fa2ba2d56a107be7a5
Ammmar <sockrat.love@gmail.com>
2021-07-13 20:45:43 +0200

    test

--- src/server/src/controllers/views.controller.ts
+++ src/server/src/controllers/views.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-const obj = {};
+
 export default class Product {
   static async renderHome(_: Request, response: Response) {
     response.render("home.hbs");
